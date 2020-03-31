5b8f41c7af74b05ca69b951cd051de635816751f
ncanna <na3au@virginia.edu>
2020-03-31 19:51:25 -0400

    Add pages and assets

--- app.py
+++ app.py
@@ -1,56 +1,38 @@
-import dash
-from dash.dependencies import Input, Output
 import dash_core_components as dcc
 import dash_html_components as html
+from dash.dependencies import Input, Output
 
-import flask
-import pandas as pd
-import time
-import os
+from pages import (
+    overview,
+    distributions,
+)
 
-server = flask.Flask('app')
-server.secret_key = os.environ.get('secret_key', 'secret')
+app = dash.Dash(
+    __name__, meta_tags=[{"name": "viewport", "content": "width=device-width"}]
+)
+server = app.server
 
-df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/hello-world-stock.csv')
+# Describe the layout/ UI of the app
+app.layout = html.Div(
+    [dcc.Location(id="url", refresh=False), html.Div(id="page-content")]
+)
 
-app = dash.Dash('app', server=server)
+# Update page
+@app.callback(Output("page-content", "children"), [Input("url", "pathname")])
+def display_page(pathname):
+    if pathname == "/distributions":
+        return distributions.create_layout(app)
+    elif pathname == "/home":
+        return (
+            overview.create_layout(app),
+            distributions.create_layout(app),
+        )
+    else:
+        return overview.create_layout(app)
 
-app.scripts.config.serve_locally = False
-dcc._js_dist[0]['external_url'] = 'https://cdn.plot.ly/plotly-basic-latest.min.js'
 
-app.layout = html.Div([
-    html.H1('Stock Tickers'),
-    dcc.Dropdown(
-        id='my-dropdown',
-        options=[
-            {'label': 'Tesla', 'value': 'TSLA'},
-            {'label': 'Apple', 'value': 'AAPL'},
-            {'label': 'Coke', 'value': 'COKE'}
-        ],
-        value='TSLA'
-    ),
-    dcc.Graph(id='my-graph')
-], className="container")
-
-@app.callback(Output('my-graph', 'figure'),
-              [Input('my-dropdown', 'value')])
-def update_graph(selected_dropdown_value):
-    dff = df[df['Stock'] == selected_dropdown_value]
-    return {
-        'data': [{
-            'x': dff.Date,
-            'y': dff.Close,
-            'line': {
-                'width': 3,
-                'shape': 'spline'
-            }
-        }],
-        'layout': {
-            'margin': {
-                'l': 30,
-                'r': 20,
-                'b': 30,
-                't': 20
+if __name__ == "__main__":
+    app.run_server(debug=True)'t': 20
             }
         }
     }
